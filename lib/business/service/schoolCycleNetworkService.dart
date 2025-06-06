5efa66032a84c89e3da852cad43a28d9292842dd
Exauce Bodi <exaucebodi4@gmail.com>
2025-06-06 14:41:02 +0200

    Creation de la classe abstraite et l'implementation pour les services

--- lib/business/service/schoolCycleNetworkService.dart
+++ lib/business/service/schoolCycleNetworkService.dart
@@ -1,1 +1,12 @@
-abstract class Schoolcycleservice {}
+import 'package:schoolcycle_mobile/business/models/announcement/announcement.dart';
+import 'package:schoolcycle_mobile/business/models/category/category.dart';
+
+abstract class SchoolcycleNetworkService {
+  // Gestion d'annonces
+  Future<List<Announcement>> getAnnouncements();
+  Future<Announcement> getAnnouncement();
+
+  // Gestion des categories
+  Future<List<Category>> getCategories();
+  Future<Category> getCategory();
+}
